212fc0b6eada7118adfc56c57923cecc6bdc2ef4
Jirayu Sirivorawong <jirayu.work@gmail.com>
2019-06-19 00:46:07 +1000

    formatted with comments

--- week02/lab-2/BigNum.c
+++ week02/lab-2/BigNum.c
@@ -2,23 +2,22 @@
 
 #include <assert.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 #include "BigNum.h"
 
-
 // Returns the number of real digits from strings
-int real_digits (char *s) {
+int real_digits(char *s) {
     int size = 0;
-    for (int i = 0, j = strlen(s), found_num = 0; i<j; i++) {
-       //check for the index of the first real number
-       if (s[i] >= '1' && s[i]<= '9' && found_num != 1) {
-           found_num = 1;
-       }
-       //count the size of digits
+    for (int i = 0, j = strlen(s), found_num = 0; i < j; i++) {
+        // check for the index of the first real number
+        if (s[i] >= '1' && s[i] <= '9' && found_num != 1) {
+            found_num = 1;
+        }
+        // count the size of digits
         if (found_num > 0 && isdigit(s[i])) {
             size++;
         }
@@ -26,30 +25,30 @@ int real_digits (char *s) {
     return size;
 }
 
-//put real digits into array, array of digit-rich
+// put real digits into array, array of digit-rich
 char *digits_to_array(char *s) {
     int size = real_digits(s);
-    //allocate space for the array
+    // allocate space for the array
     char *digits = malloc(sizeof(Byte) * size);
 
-    for (int i = 0, j = strlen(s), found_num = 0, ind = 0; i<j; i++) {
-       //check for the index of the first real number
-       if (s[i] >= '1' && s[i]<= '9' && found_num != 1) {
-           found_num = 1;
-       }
-       //pass in the digits to array
+    for (int i = 0, j = strlen(s), found_num = 0, ind = 0; i < j; i++) {
+        // check for the index of the first real number
+        if (s[i] >= '1' && s[i] <= '9' && found_num != 1) {
+            found_num = 1;
+        }
+        // pass in the digits to array
         if (found_num > 0 && isdigit(s[i])) {
             digits[ind] = s[i];
             ind++;
         }
     }
-   
+
     return digits;
 }
 
-void reverse_string (char *s) {
+void reverse_string(char *s) {
     int tmp = 0;
-    for (int start = 0, end = strlen(s) - 1; start < end; start++,end--) {
+    for (int start = 0, end = strlen(s) - 1; start < end; start++, end--) {
         tmp = s[start];
         s[start] = s[end];
         s[end] = tmp;
@@ -63,11 +62,13 @@ bool checkvalid(char *s) {
             flag = 1;
         }
     }
-    if (flag ==0) return false;
-    else return true;
+    if (flag == 0)
+        return false;
+    else
+        return true;
 }
 
-void copy_string (Byte *dest, char *source) {
+void copy_string(Byte *dest, char *source) {
     for (int i = 0, j = strlen(source); i < j; i++) {
         dest[i] = source[i];
     }
@@ -77,49 +78,48 @@ void copy_string (Byte *dest, char *source) {
 void initBigNum(BigNum *bn, int Nbytes) {
     // TODO
     bn->nbytes = Nbytes;
-    bn->bytes = calloc(Nbytes , sizeof(Byte));
+    bn->bytes = calloc(Nbytes, sizeof(Byte));
     assert(bn->bytes != NULL);
     return;
 }
 
 // Add two BigNums and store result in a third BigNum
 void addBigNums(BigNum bnA, BigNum bnB, BigNum *res) {
-    int i = 0, j = 0,sum = 0, carry = 0;
+    int i = 0, j = 0, sum = 0, carry = 0;
     char tmp[100] = {'\0'};
     res->nbytes = 0;
     while (bnA.bytes[i] != '\0' || bnB.bytes[j] != '\0') {
         int A = bnA.bytes[i] != '\0' ? bnA.bytes[i] - '0' : 0;
         int B = bnB.bytes[j] != '\0' ? bnB.bytes[j] - '0' : 0;
-        //calculate value of the digits
+        // calculate value of the digits
         sum = carry + A + B;
         /* debug */
-        //printf("this is : %i\n", A);
-        //update the carry
+        // printf("this is : %i\n", A);
+        // update the carry
         carry = (sum >= 10) ? 1 : 0;
-        //update sum if greater than 10
+        // update sum if greater than 10
         sum = sum % 10;
-        //put sum into the array
+        // put sum into the array
         tmp[i >= j ? i : j] = sum + '0';
-        //update the size of sum
+        // update the size of sum
         res->nbytes++;
-        //move to the next digit
+        // move to the next digit
         if (bnA.bytes[i] != '\0') i++;
         if (bnB.bytes[j] != '\0') j++;
     }
+    //add carry
     if (carry > 0) {
         tmp[res->nbytes] = '1';
         res->nbytes++;
     }
+    //re-allocate space if the size required is more than 20
     if (res->nbytes > 20) {
         res->bytes = realloc(res->bytes, sizeof(Byte) * strlen(tmp));
     }
     copy_string(res->bytes, tmp);
 
-
     return;
 }
-
-
 
 // Set the value of a BigNum from a string of digits
 // Returns 1 if it *was* a string of digits, 0 otherwise
@@ -127,10 +127,10 @@ int scanBigNum(char *s, BigNum *bn) {
     /*check if it exceed previously allocated space*/
     int size = real_digits(s);
     if (size > 20) {
-         bn->bytes = realloc(bn->bytes, size * sizeof(Byte));
+        bn->bytes = realloc(bn->bytes, size * sizeof(Byte));
     }
     if (!checkvalid(s)) return 0;
-    char* digits = digits_to_array(s);
+    char *digits = digits_to_array(s);
     reverse_string(digits);
     bn->bytes = (Byte *)strcpy((char *)bn->bytes, digits);
     bn->nbytes = size;
@@ -140,8 +140,7 @@ int scanBigNum(char *s, BigNum *bn) {
 
 // Display a BigNum in decimal format
 void showBigNum(BigNum bn) {
-    
-    //printing backwards
+    // printing backwards
     for (int start = 0, end = bn.nbytes - 1; end >= start; end--) {
         printf("%c", bn.bytes[end]);
     }
